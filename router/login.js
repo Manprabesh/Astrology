2686da2ec2c3bf78ad143141e77043fb99c7c00c
Manprabesh <billionaireboruah31@gmail.com>
2025-03-08 23:45:59 +0530

    password hashed

--- router/login.js
+++ router/login.js
@@ -2,6 +2,7 @@ import express from 'express'
 const loginRouter = express.Router()
 import pool from '../database.js'
 import JsonWebToken from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import createBloggingSchema from '../models/bloggingModel.js'
 import createUserSchema from '../models/userModel.js'
 
@@ -11,28 +12,40 @@ loginRouter.post('/login', (req, res) => {
 
     pool.query(createUserSchema)
 
-    const data=pool.query('SELECT email FROM userAccount WHERE email=$1', [email], (err, result) => {
+    const data = pool.query('SELECT email,password FROM userAccount WHERE email=$1', [email], (err, result) => {
 
-        if(!result){
+        if (!result) {
             return res.json("user don't exist")
         }
+
+        const hash=result.rows[0].password;
+
 
         if (result.rows[0]) {
 
             console.log(result.rows);
 
             let token = JsonWebToken.sign({ email: email }, 'secretkey');
-            res.cookie("token", token)
-            console.log(token);
 
-           res.json("User exist")
+            bcrypt.compare(password, hash, function (err, result) {
+                if (result) {
+                    res.cookie("token", token)
+                    console.log(token);
+
+                    return res.json("User exist")
+                }
+                else {
+                    return res.json("User don't exist")
+                }
+            });
+
 
         }
         else {
-            res.json("User don't exist")
+          return  res.json("User don't exist")
         }
     })
-    console.log(data,"data");
+    console.log(data, "data");
 
 })
 
